de7f1e9189927599dbe8fa3a06d601488d337eeb
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2025-03-23 22:46:02 +0000

    fix: Change WebSocket URL to use HTTPS

--- frontend/websocket.js
+++ frontend/websocket.js
@@ -12,7 +12,7 @@ const RECONNECTION_DELAY = 3;
 function getWebSocketServer() {
     const host = window.location.host;
     if (host === "draw.nikolausbrock.no") {
-        return "ws://draw.nikolausbrock.no/ws/"
+        return "wss://draw.nikolausbrock.no/ws/"
     } else if (host.startsWith("localhost")) {
         return "ws://192.168.247.133:8001";
     } else {
